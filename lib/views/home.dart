86d2041786cae7b9f22c9405cbd7e256ef65156e
ojas010 <ojasbhanarkar010@gmail.com>
2022-07-09 09:28:40 +0530

    first commit

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -14,7 +14,6 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   late Razorpay razorpay;
 
   TextEditingController textEditingController = new TextEditingController();
@@ -24,7 +23,7 @@ class _HomeState extends State<Home> {
     super.initState();
 
     razorpay = new Razorpay();
-    
+
     razorpay.on(Razorpay.EVENT_PAYMENT_SUCCESS, handlerPaymentSuccess);
     razorpay.on(Razorpay.EVENT_PAYMENT_ERROR, handlerErrorFailure);
     razorpay.on(Razorpay.EVENT_EXTERNAL_WALLET, handlerExternalWallet);
@@ -36,39 +35,36 @@ class _HomeState extends State<Home> {
     razorpay.clear();
   }
 
-  void openCheckout(){
+  void openCheckout() {
     var options = {
-      "key" : "rzp_test_xQTRWbhbn6Uzaa",
-      "amount" : num.parse(textEditingController.text)*100,
-      "name" : "Sample",
-      "description" : "Payment for product",
-      "prefill" : {
-        "contact" : "2323232323",
-        "email" : "thisisatest@gmail.com"
-      },
-      "external" : {
-        "wallets" : ["paytm"]
+      "key": "rzp_test_gWTUXYM761KGVl",
+      "amount": num.parse(textEditingController.text) * 100,
+      "name": "Sample",
+      "description": "Payment for product",
+      "prefill": {"contact": "2323232323", "email": "thisisatest@gmail.com"},
+      "external": {
+        "wallets": ["paytm"]
       }
     };
 
-    try{
+    try {
       razorpay.open(options);
-    }catch(e){
+    } catch (e) {
       print(e.toString());
     }
   }
 
-  void handlerPaymentSuccess(){
+  void handlerPaymentSuccess() {
     print("Payment Successful");
     Fluttertoast.showToast(msg: "Payment Successful");
   }
 
-  void handlerErrorFailure(){
+  void handlerErrorFailure() {
     print("Payment Failed");
     Fluttertoast.showToast(msg: "Payment Failed");
   }
 
-  void handlerExternalWallet(){
+  void handlerExternalWallet() {
     print("External Wallet");
     Fluttertoast.showToast(msg: "External Wallet");
   }
@@ -77,64 +73,62 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: Drawer(
-        child: ListView(
-          children: <Widget>[
-            const SizedBox(
-              height: 56.0,
-              child: const DecoratedBox(
-                  decoration: const BoxDecoration(
-                    color: Colors.redAccent,
-                  )
-              ),
-            ),
-            SizedBox(
-              height: 15.0,
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.person,
-                size: 26.0,
-              ),
-              title: const Text('Account', style: TextStyle(fontSize: 18.0)),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Account()),
-                );
-              },
-            ),
-            new Divider(
-              thickness: 1.0,
+          child: ListView(
+        children: <Widget>[
+          const SizedBox(
+            height: 56.0,
+            child: const DecoratedBox(
+                decoration: const BoxDecoration(
               color: Colors.redAccent,
+            )),
+          ),
+          SizedBox(
+            height: 15.0,
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.person,
+              size: 26.0,
             ),
-            ListTile(
-                leading: Icon(
-                  Icons.home,
-                  size: 26.0,
-                ),
-              title: const Text('Address', style: TextStyle(fontSize: 18.0)),
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Address()),
-                );
-              },
+            title: const Text('Account', style: TextStyle(fontSize: 18.0)),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Account()),
+              );
+            },
+          ),
+          new Divider(
+            thickness: 1.0,
+            color: Colors.redAccent,
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.home,
+              size: 26.0,
             ),
-            new Divider(
-              thickness: 1.0,
-              color: Colors.redAccent,
+            title: const Text('Address', style: TextStyle(fontSize: 18.0)),
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => Address()),
+              );
+            },
+          ),
+          new Divider(
+            thickness: 1.0,
+            color: Colors.redAccent,
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.logout,
+              size: 26.0,
             ),
-            ListTile(
-              leading: Icon(
-                Icons.logout,
-                size: 26.0,
-              ),
-              title: const Text('Logout', style: TextStyle(fontSize: 18.0)),
-              onTap: () => exit(0),
-            ),
-          ],
-        )
-      ),
+            title: const Text('Logout', style: TextStyle(fontSize: 18.0)),
+            onTap: () => exit(0),
+          ),
+        ],
+      )),
       appBar: AppBar(
         title: Center(child: Text("")),
       ),
@@ -144,20 +138,18 @@ class _HomeState extends State<Home> {
           children: <Widget>[
             TextField(
               controller: textEditingController,
-              decoration: InputDecoration(
-                hintText: "Enter the amount"
-              ),
+              decoration: InputDecoration(hintText: "Enter the amount"),
             ),
             SizedBox(height: 12),
             RaisedButton(
-              color: Colors.blue,
-              child: Text("Pay Now", style: TextStyle(
-                color: Colors.white,
-              )),
-              onPressed: () {
-                openCheckout();
-              }
-            )
+                color: Colors.blue,
+                child: Text("Pay Now",
+                    style: TextStyle(
+                      color: Colors.white,
+                    )),
+                onPressed: () {
+                  openCheckout();
+                })
           ],
         ),
       ),
